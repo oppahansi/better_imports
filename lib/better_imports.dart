a963d48b779c588957ba6e13662608ede0c97caf
oppahansi <oppahansi@gmail.com>
2023-12-10 16:15:41 +0100

    chore: added recommended lint rules

--- lib/better_imports.dart
+++ lib/better_imports.dart
@@ -1,6 +1,6 @@
 library better_imports;
 
+export 'constants.dart';
 export 'parser.dart';
 export 'print.dart';
 export 'sort_cmd.dart';
-export 'constants.dart';
--- test/better_imports_test.dart
+++ test/better_imports_test.dart
@@ -1,34 +1,34 @@
-// Dart Imports
-import 'dart:async';
-import 'dart:io';
-import 'dart:js';
+// // Dart Imports
+// import 'dart:async';
+// import 'dart:io';
+// import 'dart:js';
 
-// Flutter Imports
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/local_notifications/flutter_local_notifications.dart'
-    hide PendingNotificationRequest;
-import 'package:flutter/material.dart';
-import 'package:flutter/painting.dart' as painting;
-import 'package:flutter/physics.dart';
+// // Flutter Imports
+// import 'package:flutter/cupertino.dart';
+// import 'package:flutter/local_notifications/flutter_local_notifications.dart'
+//     hide PendingNotificationRequest;
+// import 'package:flutter/material.dart';
+// import 'package:flutter/painting.dart' as painting;
+// import 'package:flutter/physics.dart';
 
-// Package Imports
-import 'package:intl/intl.dart';
-import 'package:mdi/mdi.dart';
-import 'package:provider/provider.dart';
+// // Package Imports
+// import 'package:intl/intl.dart';
+// import 'package:mdi/mdi.dart';
+// import 'package:provider/provider.dart';
 
-// Project Imports
-import 'package:better_imports/anotherFile2.dart';
+// // Project Imports
+// import 'package:better_imports/anotherFile2.dart';
 
-// Relative Project Imports
-import 'anotherFile.dart';
+// // Relative Project Imports
+// import 'anotherFile.dart';
 
-/// asdf
-void main() {
-  var test = ";";
+// /// asdf
+// void main() {
+//   var test = ";";
 
-  var more = 1;
+//   var more = 1;
 
-  // ahahahs
+//   // ahahahs
 
-  var brre = "  ;";
-}
+//   var brre = "  ;";
+// }
