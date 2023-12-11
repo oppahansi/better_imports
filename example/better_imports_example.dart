c675d1dde39347c8d1c38c61f66c493340bb7b21
oppahansi <oppahansi@gmail.com>
2023-12-11 16:51:24 +0100

    chore: refactoring

--- example/better_imports_example.dart
+++
@@ -1,6 +0,0 @@
-// import 'package:better_imports/better_imports.dart';
-
-// void main() {
-//   var awesome = BetterImports();
-//   print('awesome: ${awesome.isAwesome}');
-// }
--- lib/imports/import_item.dart
+++
@@ -1,3 +0,0 @@
-class Import {
-  List<String> lines = [];
-}
