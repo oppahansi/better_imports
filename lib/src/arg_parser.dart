fc52dcaf2c9d4b7a70ffda30b770b4201474427c
oppahansi <oppahansi@protonmail.com>
2025-06-03 18:25:23 +0200

    style: Refactor code formatting for improved readability in arg_parser.dart, cfg.dart, and files_sorter.dart

--- lib/src/arg_parser.dart
+++ lib/src/arg_parser.dart
@@ -4,43 +4,42 @@ import 'package:args/args.dart';
 // Project Imports
 import 'package:better_imports/src/constants.dart';
 
-final argParser =
-    ArgParser()
-      ..addFlag(
-        Constants.helpFlag,
-        abbr: Constants.helpFlagAbbr,
-        negatable: false,
-      )
-      ..addFlag(
-        Constants.recursiveFlag,
-        abbr: Constants.recursiveFlagAbbr,
-        defaultsTo: true,
-        negatable: true,
-      )
-      ..addFlag(
-        Constants.commentsFlag,
-        abbr: Constants.commentsFlagAbbr,
-        defaultsTo: true,
-        negatable: true,
-      )
-      ..addFlag(
-        Constants.silentFlag,
-        abbr: Constants.silentFlagAbbr,
-        defaultsTo: false,
-        negatable: false,
-      )
-      ..addFlag(Constants.relativeFlag, defaultsTo: false, negatable: false)
-      ..addFlag(Constants.traceFlag, defaultsTo: false, negatable: false)
-      ..addFlag(Constants.dryRunFlag, defaultsTo: false, negatable: false)
-      ..addFlag(Constants.dartFmt, defaultsTo: true, negatable: true)
-      ..addOption(Constants.cfgPathOption, mandatory: false)
-      ..addOption(Constants.projectNameOption, defaultsTo: "", mandatory: false)
-      ..addOption(Constants.foldersOption, defaultsTo: "", mandatory: false)
-      ..addOption(Constants.filesOption, defaultsTo: "", mandatory: false)
-      ..addOption(Constants.ignoreFilesOption, defaultsTo: "", mandatory: false)
-      ..addOption(Constants.filesLikeOption, defaultsTo: "", mandatory: false)
-      ..addOption(
-        Constants.ignoreFilesLikeOption,
-        defaultsTo: "",
-        mandatory: false,
-      );
+final argParser = ArgParser()
+  ..addFlag(
+    Constants.helpFlag,
+    abbr: Constants.helpFlagAbbr,
+    negatable: false,
+  )
+  ..addFlag(
+    Constants.recursiveFlag,
+    abbr: Constants.recursiveFlagAbbr,
+    defaultsTo: true,
+    negatable: true,
+  )
+  ..addFlag(
+    Constants.commentsFlag,
+    abbr: Constants.commentsFlagAbbr,
+    defaultsTo: true,
+    negatable: true,
+  )
+  ..addFlag(
+    Constants.silentFlag,
+    abbr: Constants.silentFlagAbbr,
+    defaultsTo: false,
+    negatable: false,
+  )
+  ..addFlag(Constants.relativeFlag, defaultsTo: false, negatable: false)
+  ..addFlag(Constants.traceFlag, defaultsTo: false, negatable: false)
+  ..addFlag(Constants.dryRunFlag, defaultsTo: false, negatable: false)
+  ..addFlag(Constants.dartFmt, defaultsTo: true, negatable: true)
+  ..addOption(Constants.cfgPathOption, mandatory: false)
+  ..addOption(Constants.projectNameOption, defaultsTo: "", mandatory: false)
+  ..addOption(Constants.foldersOption, defaultsTo: "", mandatory: false)
+  ..addOption(Constants.filesOption, defaultsTo: "", mandatory: false)
+  ..addOption(Constants.ignoreFilesOption, defaultsTo: "", mandatory: false)
+  ..addOption(Constants.filesLikeOption, defaultsTo: "", mandatory: false)
+  ..addOption(
+    Constants.ignoreFilesLikeOption,
+    defaultsTo: "",
+    mandatory: false,
+  );
