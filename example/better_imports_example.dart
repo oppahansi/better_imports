c2d9ff9b63c1ea58a64bdd193c4f998ada8bd428
oppahansi <oppahansi@protonmail.com>
2025-06-09 09:37:39 +0200

    chore: update description in pubspec.yaml for better clarity and add example file

---
+++ example/better_imports_example.dart
@@ -0,0 +1,7 @@
+void main() {
+  // This package is not inteded to be used directly.
+  // Instead, use the either `better_imports` package to manage imports
+  // or the `better_imports` binaries to automatically fix imports in your Dart files.
+
+  // Follow the readme instructions to set up the `better_imports` package in your project.
+}
